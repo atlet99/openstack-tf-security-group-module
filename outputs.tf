32268e8f11573728f04dbf678d7056267d12e534
Abdurakhman R. <joha.shadibekov@gmail.com>
2024-11-08 21:32:07 +0500

    [DOT-368] - fix security group ID reference in outputs to resolve indexing issue;

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "security_group_id" {
   description = "The ID of the security group"
-  value       = try(openstack_networking_secgroup_v2.this[0].id, "")
+  value       = try(openstack_networking_secgroup_v2.this["id"].id, "")
 }
 
 output "security_group_name" {
